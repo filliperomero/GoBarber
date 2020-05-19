e8c9a8550d25070dca7f4ecb1eb99d6b5f88803e
filliperomero <fillipe.romero@gmail.com>
2020-05-19 16:05:00 -0300

    chore: add user route to create and change provider to provider_id

--- backend/src/services/CreateAppointmentService.ts
+++ backend/src/services/CreateAppointmentService.ts
@@ -5,12 +5,15 @@ import Appointment from '../models/Appointment';
 import AppointmentsRepository from '../repositories/AppointmentsRepository';
 
 interface RequestDTO {
-  provider: string;
+  provider_id: string;
   date: Date;
 }
 
 class CreateAppointmentService {
-  public async execute({ date, provider }: RequestDTO): Promise<Appointment> {
+  public async execute({
+    date,
+    provider_id,
+  }: RequestDTO): Promise<Appointment> {
     const appointmentsRepository = getCustomRepository(AppointmentsRepository);
 
     const appointmentDate = startOfHour(date);
@@ -24,7 +27,7 @@ class CreateAppointmentService {
     }
 
     const appointment = appointmentsRepository.create({
-      provider,
+      provider_id,
       date: appointmentDate,
     });
 
