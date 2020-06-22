a40a5c395345ea7748edd3c1a1a13e6bc84dae05
filliperomero <fillipe.romero@gmail.com>
2020-06-21 21:13:12 -0300

    chore: remove unnecessary parse date since joi already does that

--- backend/src/modules/appointments/infra/http/controllers/AppointmentsController.ts
+++ backend/src/modules/appointments/infra/http/controllers/AppointmentsController.ts
@@ -1,5 +1,4 @@
 import { Request, Response } from 'express';
-import { parseISO } from 'date-fns';
 import { container } from 'tsyringe';
 
 import CreateAppointmentService from '@modules/appointments/services/CreateAppointmentService';
@@ -8,14 +7,12 @@ export default class AppointmentsController {
   public async create(req: Request, res: Response): Promise<Response> {
     const { provider_id, date } = req.body;
 
-    const parsedDate = parseISO(date);
-
     const createAppointment = container.resolve(CreateAppointmentService);
 
     const appointment = await createAppointment.execute({
       provider_id,
       user_id: req.user.id,
-      date: parsedDate,
+      date,
     });
 
     return res.json(appointment);
