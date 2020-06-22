64a9f2e7af2d1fa0f9def59735622225c9687ab6
filliperomero <fillipe.romero@gmail.com>
2020-06-22 16:16:15 -0300

    chore: add header to dashboard page

--- frontend/src/pages/Dashboard/index.tsx
+++ frontend/src/pages/Dashboard/index.tsx
@@ -1,10 +1,34 @@
 import React from 'react';
+import { FiPower } from 'react-icons/fi';
 
-import { Container } from './styles';
+import { useAuth } from '../../hooks/Auth';
+
+import logo from '../../assets/logo.svg';
+
+import { Container, Header, HeaderContent, Profile } from './styles';
 
 const Dashboard: React.FC = () => {
+  const { signOut, user } = useAuth();
+
   return (
     <Container>
+      <Header>
+        <HeaderContent>
+          <img src={logo} alt="GoBarber" />
+
+          <Profile>
+            <img src={user.avatar_url} alt={user.name} />
+            <div>
+              <span>Bem-vindo,</span>
+              <strong>{user.name}</strong>
+            </div>
+          </Profile>
+
+          <button type="button" onClick={signOut}>
+            <FiPower />
+          </button>
+        </HeaderContent>
+      </Header>
       <h1>Dashboard</h1>
     </Container>
   );
