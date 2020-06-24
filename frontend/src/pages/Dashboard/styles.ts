6e0a69e137273228a31a3d4b5ba183e5f1d458b7
filliperomero <fillipe.romero@gmail.com>
2020-06-24 16:21:10 -0300

    chore: add profile link in the dashboard

--- frontend/src/pages/Dashboard/styles.ts
+++ frontend/src/pages/Dashboard/styles.ts
@@ -52,8 +52,14 @@ export const Profile = styled.div`
       color: #f4ede8;
     }
 
-    strong {
+    a {
+      text-decoration: none;
       color: #ff9000;
+      transition: opacity 0.2s;
+
+      &:hover {
+        opacity: 0.8;
+      }
     }
   }
 `;
